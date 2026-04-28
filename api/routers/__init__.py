ef45e9c3fe1d618c82b84da1c5fb5e10addb9ea7
Smart research <smartresearch2026@163.com>
2026-04-28 18:20:01 +0800

    Add api/routers/__init__.py

---
+++ api/routers/__init__.py
@@ -0,0 +1,8 @@
+from .auth import router as auth_router
+from .users import router as users_router
+from .matching import router as matching_router
+from .messages import router as messages_router
+from .health import router as health_router
+from .payments import router as payments_router
+from .reports import router as reports_router
+from .ai_advisor import router as ai_advisor_router
